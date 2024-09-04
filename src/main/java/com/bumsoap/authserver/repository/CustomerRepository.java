cda52ff2ea4e82ebd7118fdb857dfecdf1987328
osparking <jbpark03@gmail.com>
2024-09-04 14:07:08 +0900

    make Customer Repository interface - findbyEmail

---
+++ src/main/java/com/bumsoap/authserver/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.bumsoap.authserver.repository;
+
+import com.bumsoap.authserver.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+}
