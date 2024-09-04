3e8c3859658b834daefc86004ee6c64f8a0e670b
osparking <jbpark03@gmail.com>
2024-09-04 13:59:03 +0900

    make UserDetailsService bean which uses CustomerRepository

---
+++ src/main/java/com/bumsoap/authserver/config/MyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.bumsoap.authserver.config;
+
+import com.bumsoap.authserver.model.Customer;
+import com.bumsoap.authserver.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("다음 유저 정보는 존재하지 않음: " + username));
+        List<GrantedAuthority> authorities = customer.getAuthorities().stream().map(authority -> new
+                SimpleGrantedAuthority(authority.getName())).collect(Collectors.toList());
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
